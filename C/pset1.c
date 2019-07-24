dbd22603e1341ad6591af0477c6184650a050c2f
mrkwapo <mrkwapo@gmail.com>
2019-07-24 15:48:02 -0400

    completed hello section

--- C/pset1.c
+++ C/pset1.c
@@ -5,4 +5,6 @@ int main(void)
 {
     string name = get_string("What is your name?\n");
     printf("hello, %s\n", name);
+    string status = get_string("\n");
+    printf("%s, same here. \n", status);
 }
