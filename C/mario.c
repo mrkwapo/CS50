19cc1b59d962f2570889414e0abe09093b087535
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-07-27 23:41:46 -0400

    Update mario.c

--- C/mario.c
+++ C/mario.c
@@ -1,24 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <unistd.h>
+
+int get_positive_int(string prompt);
 
 int main(void)
 {
-    string firstname = get_string("What is your first name?\n");
-    string lastname = get_string("%s, what is your last name?\n", firstname);
-    int y = 8;
- int height = get_int("Ok %s %s, what is the height of your pyramid? (must be between 1 and 8)\n", firstname, lastname);
-    if(height <= y && height >= 0)
+    int height = get_positive_int("Height: ");
+    
+    for(int i= 0; i < height; i++)
     {
-        for ( int i = 0; i < height; i+=1)
-        {
-            printf("#\n");
-            sleep(1);
-         }
-    } 
-    else {
-        printf(" %s %s, %i is not between 1 and 8. Try again, silly!\n", firstname, lastname, height);
+        for (int j = i; j < height; j++)
+        {     
+        printf("#");
+        }
+        printf("\n");
     }
+}
 
-
+// Prompt user for positive integer
+int get_positive_int(string prompt)
+{
+    int n;
+    do
+    {
+        n = get_int("%s", prompt);
+    }
+//     
+    while (n > 8 || n < 1);
+    return n;
 }
