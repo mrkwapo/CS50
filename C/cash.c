6f9bd72ca20dd058151b2d0bc3f4f228e449087e
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-07-29 18:19:28 -0400

    Update cash.c

--- C/cash.c
+++ C/cash.c
@@ -5,15 +5,41 @@ float get_positive_float(string prompt);
 
 int main(void)
 {
-    float change = get_positive_float("Change Due: $");
+    float change = get_positive_float("Change Due: $"); 
+    float quarter = 0.25;
+    int quarters = (change/quarter);
+    float qremainder = (change-quarters*quarter);
+    float dime = 0.10;
+    int dimes = (qremainder/dime);
+    float dremainder = (qremainder-dimes*dime);
+    float nickel = 0.05;
+    int nickels = (dremainder/nickel);
+    float nremainder = (dremainder-nickels*nickel);
+    float penny = 0.01;
+    int pennies = (nremainder/penny);
+    float premainder = (nremainder-pennies*penny);
+
     
     {
-        printf("Change owed: $%.2f\n", change);
+
+        printf("Amount of Quarters Due: %i\n", quarters);
+        if(qremainder != .24 && qremainder < .24) {
+            printf("Amount of Dimes Due: %.0f\n", qremainder/dime);
+        }
+        if(dremainder > 1 ) { 
+            printf("Amount of Nickels Due: %.1f\n", dremainder/nickel);
+        }
+
+        if(nremainder != 0 ) { 
+            printf("Amount of Pennies Due: %.0f\n", nremainder/penny);
+        }
+
+
     }
 
 }
 
-// Prompt user for positive integer
+
 float get_positive_float(string prompt)
 {
     float n;
@@ -22,6 +48,6 @@ float get_positive_float(string prompt)
         n = get_float("%s", prompt);
     }
 //     
-    while (n <= .00);
+    while (n <= 0);
     return n;
 }
