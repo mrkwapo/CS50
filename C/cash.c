5fefbc7bf9a6012284bf13b598d5ecc8a03e6a76
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-07-30 20:22:42 -0400

    Update cash.c

--- C/cash.c
+++ C/cash.c
@@ -1,42 +1,40 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 float get_positive_float(string prompt);
 
 int main(void)
 {
-    float change = get_positive_float("Change Due: $"); 
-    float quarter = 0.25;
-    int quarters = (change/quarter);
-    float qremainder = (change-quarters*quarter);
-    float dime = 0.10;
-    int dimes = (qremainder/dime);
-    float dremainder = (qremainder-dimes*dime);
-    float nickel = 0.05;
-    int nickels = (dremainder/nickel);
-    float nremainder = (dremainder-nickels*nickel);
-    float penny = 0.01;
-    int pennies = (nremainder/penny);
-    float premainder = (nremainder-pennies*penny);
-
+float change = get_positive_float("Change owed: $");
+int convertedchange = round(change * 100);
+int quarter = 25;
+int dime = 10;
+int penny = 1;
+    int count = 0;
     
-    {
-
-        printf("Amount of Quarters Due: %i\n", quarters);
-        if(qremainder != .24 && qremainder < .24) {
-            printf("Amount of Dimes Due: %.0f\n", qremainder/dime);
-        }
-        if(dremainder > 1 ) { 
-            printf("Amount of Nickels Due: %.1f\n", dremainder/nickel);
-        }
-
-        if(nremainder != 0 ) { 
-            printf("Amount of Pennies Due: %.0f\n", nremainder/penny);
-        }
-
-
-    }
-
+ while (convertedchange >=25)
+ {
+     count += (convertedchange/25);
+     convertedchange %= 25;
+ }
+   while (convertedchange >=10)
+ {
+     count += (convertedchange/10);
+     convertedchange %= 10;
+ }
+   while (convertedchange >=5)
+ {
+     count += (convertedchange/5);
+     convertedchange %= 5;
+ }
+   while (convertedchange >=1)
+ {
+     count += (convertedchange/1);
+     convertedchange %= 1;
+ }
+  printf("%i\n", count);
+    
 }
 
 
