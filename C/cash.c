042c56ef02d986cb8d70a6ac080801131436f7c2
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-07-31 11:42:47 -0400

    Update cash.c

--- C/cash.c
+++ C/cash.c
@@ -1,7 +1,9 @@
+//This program is the Greedy Algorithm. When a user inputs an amount of change owed in USD, the program returns the smallest amount of coins that can be used to pay in increments of .25, .10, .05 and .01 cents.
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
 
+//Declares that this function exists
 float get_positive_float(string prompt);
 
 int main(void)
@@ -15,26 +17,27 @@ int count = 0;
      count += (convertedchange/25);
      convertedchange %= 25;
  }
-   while (convertedchange >=10 && convertedchange < 25)
+ while (convertedchange >=10 && convertedchange < 25)
  {
      count += (convertedchange/10);
      convertedchange %= 10;
  }
-   while (convertedchange >=5 && convertedchange < 10)
+ while (convertedchange >=5 && convertedchange < 10)
  {
      count += (convertedchange/5);
      convertedchange %= 5;
  }
-   while (convertedchange >=1 && convertedchange < 5)
+ while (convertedchange >=1 && convertedchange < 5)
  {
      count += (convertedchange/1);
      convertedchange %= 1;
- }
-  printf("%i\n", count);
+}
+    
+printf("%i\n", count);
     
 }
 
-
+//Prompts user to submit a positive float
 float get_positive_float(string prompt)
 {
     float n;
@@ -42,7 +45,7 @@ float get_positive_float(string prompt)
     {
         n = get_float("%s", prompt);
     }
-//     
+    
     while (n <= 0);
     return n;
 }
