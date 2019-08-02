d02ea08c1e8d04bfafbf4306efe6d359bcaf23c5
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-01 20:27:47 -0400

    Update score1b.c

--- C/score1b.c
+++ C/score1b.c
@@ -5,25 +5,25 @@ void chart(int score);
 int main(void)
 {
     //Get number of scores to record from user
-    int j = get_int("how many scores to record: ");
+    const int COUNT = get_int("how many scores to record: ");
     
     //Get scores from user
-    int scores[j];
-    for(int i = 0; i < j; i++)
+    int scores[COUNT];
+    for (int i = 0; i < COUNT; i++)
     {
         scores[i] = get_int("Score %i: ", i + 1);
     }
     //Chart Scores
-    for( int i = 0; i < j; i++)
+    for (int i = 0; i < COUNT; i++)
     {
-         printf("Score %i: ", i + 1);
-         chart(scores[i]);   
+        printf("Score %i: ", i + 1);
+        chart(scores[i]);   
     }  
 }
 //generating bar
 void chart(int score)
 {
-    for(int i = 0; i < score; i++)
+    for (int i = 0; i < score; i++)
     {
         printf("#");
     }
