da77e1782dcaee50951c5b124879597681ee33b4
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-05 18:15:37 -0400

    Create capitalize0.c

---
+++ C/capitalize0.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    //Ask the user for a word
+    string s = get_string("Before: ");
+    printf("After: ");
+    for(int i = 0, n = strlen(s); i < n; i++)
+    {
+        //If the ith character is a lowercase letter between a and z print it capitalized
+     if(s[i] >= 'a' && s[i] <= 'z')   
+     {
+         printf("%c", s[i] - ('a'-'A'));
+     }
+     else
+     {
+         // if the ith charater is already capitalized then print it
+         printf("%c", s[i]);
+     }
+    }
+    printf("\n");
+}
