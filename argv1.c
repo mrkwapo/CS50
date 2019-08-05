468f8fc86a97e58ad9da30e0737851ad9912886e
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-05 19:35:24 -0400

    Create argv1.c

---
+++ argv1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(int argc, string argv[])
+{
+//for every iteration print the word and new line out at the ith index position (argv[i]) 
+for(int i = 0; i < argc; i++)
+{
+    printf("%s\n", argv[i]);
+}
+
+}
