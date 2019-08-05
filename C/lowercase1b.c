e1a7f104de904b83a5d85a28dfea8a5afcf6001e
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-05 19:00:50 -0400

    Create lowercase1b.c

---
+++ C/lowercase1b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    //Ask the user for a word
+    string s = get_string("Before: ");
+    printf("After: ");
+    for(int i = 0, n = strlen(s); i < n; i++)
+    //the toupper method will lowercase any letter that is not lowercase and will not modify the letter that is lowercase
+     {
+         printf("%c", tolower(s[i]));
+     }
+     
+    
+    printf("\n");
+}
