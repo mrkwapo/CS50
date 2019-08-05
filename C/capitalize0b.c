32eca00df0d8838972511ab3c50318eb9530a8cb
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-05 18:26:39 -0400

    Create capitalize0b.c

---
+++ C/capitalize0b.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    //Ask the user for a word
+    string s = get_string("Before: ");
+    printf("After: ");
+    for(int i = 0, n = strlen(s); i < n; i++)
+    {
+        //If the ith character is a lowercase letter between a and z print it capitalized
+     if(s[i] >= 'a' && s[i] <= 'z')   
+     {
+         //print the ith character minus the difference between the character a and A (on the ASCII table which is 32)
+         printf("%c", s[i] - 32);
+     }
+     else
+     {
+         // if the ith charater is already capitalized then print the character
+         printf("%c", s[i]);
+     }
+    }
+    printf("\n");
+}
