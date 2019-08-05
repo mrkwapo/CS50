0e31caa4e8697ac7c9916eb67f1a399969578c11
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-05 18:25:09 -0400

    Update capitalize0.c

--- C/capitalize0.c
+++ C/capitalize0.c
@@ -12,6 +12,7 @@ int main(void)
         //If the ith character is a lowercase letter between a and z print it capitalized
      if(s[i] >= 'a' && s[i] <= 'z')   
      {
+         //print the ith character minus the difference between the character a and A (on the ASCII table which is 32)
          printf("%c", s[i] - ('a'-'A'));
      }
      else
