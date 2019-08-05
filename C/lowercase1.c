581a3f45a887f172fd7c3914f7d5453d8471bea3
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-05 18:41:45 -0400

    Create lowercase1.c

---
+++ C/lowercase1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    //Ask the user for a word
+    string s = get_string("Before: ");
+    printf("After: ");
+    for(int i = 0, n = strlen(s); i < n; i++)
+    {
+        //If the ith character is a lowercase letter between a and z print it capitalized
+     if(isupper(s[i]))   
+     {
+         //print the ith character minus the difference between the character a and A (on the ASCII table which is 32)
+         printf("%c", tolower(s[i]));
+     }
+     else
+     {
+         // if the ith charater is already capitalized then print it
+         printf("%c", s[i]);
+     }
+    }
+    printf("\n");
+}
