83f5826d668be6a01415ffdad1863c37253a02f0
mrkwapo <mrkwapo@gmail.com>
2019-08-06 00:57:57 -0400

    more c programs

---
+++ exit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+  if (argc != 2)
+  {
+    printf("missing command-line argument\n");
+    return 1;
+  }
+  printf("hello, %s\n", argv[1])
+}
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+  if (argc != 2)
