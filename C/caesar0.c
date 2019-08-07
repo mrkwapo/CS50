546366b9d7dc1757dd19575605fe06a098a90e65
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-06 20:06:25 -0400

    updated first draft

--- C/caesar0.c
+++ C/caesar0.c
@@ -6,6 +6,7 @@ int main(void)
 {
     //Ask the user for a word in plaint text
     string s = get_string("plaintext: ");
+    int k = get_int("key: ");
     //Print the ciphertext here 
     printf("ciphertext: ");
     for (int i = 0, n = strlen(s); i < n; i++)
@@ -14,14 +15,18 @@ int main(void)
 
      if (s[i] >= 'a' && s[i] <= 'z')   
      {         
-         printf("%c", s[i] + 1);
+         printf("%c", s[i] + k);
      }
      else if (s[i] >= 'A' && s[i] <= 'Z')   
      {
          //if its capital print the ith character plus one (based on the ASCII table)
-         printf("%c", s[i] + 1);
+         printf("%c", s[i] + k);
      }
-
+   else
+     {
+         // print if the ith charater is a space or special character
+         printf("%c", s[i]);
+     }
     }
     printf("\n");
 }
