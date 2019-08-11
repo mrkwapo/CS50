5482c395391fbcf7fe06cd214989ea43fcf8c86e
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-11 00:00:15 -0400

    Create vigenere.c

---
+++ C/vigenere.c
@@ -0,0 +1,65 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+bool validate_key(string key);
+
+int main(int argc, string argv[])
+{
+    if (!(argc == 2 && validate_key(argv[1])))
+    {
+        printf("Incorrect usage! \nTry: ./filename key \n(hint: key must be a letter )\n");
+        return 1;
+    }
+    //Because argv[1] is an element in an array of strings we need to convert the type to integer
+    string key = argv[1];
+    
+    string plaintext;
+    do
+    {
+        
+        plaintext = get_string("plaintext: ");
+    }
+    while (plaintext == NULL);
+    
+    //Itereate through plaintext to generate the ciphertext
+    printf("ciphertext: ");
+    for (int i = 0, k = 0, len = strlen(plaintext), lenk = strlen(key); i < len; i++)
+    {
+        if (isupper(plaintext[i])) // Z - 'A' + key % 26
+        {
+            printf("%c", ((plaintext[i] - 'A' + toupper(key[k]) - 'A') % 26 + 'A'));
+            // IMP PART
+            k = (k + 1) % lenk; 
+        }
+        else if (islower(plaintext[i]))
+        {
+            printf("%c", ((plaintext[i] - 'a' + tolower(key[k]) - 'a') % 26 + 'a'));
+            k = (k + 1) % lenk; 
+       
+        }
+        else
+        {
+            printf("%c", plaintext[i]);
+            
+        }
+    }
+    printf("\n");
+}
+
+bool validate_key(string key)
+{
+    
+    for (int i = 0, len = strlen(key); i < len; i++)
+    { 
+        // if the ith character is not an alphabet
+        if (!(isalpha(key[i])))
+        {
+            return false;  
+        }
+               
+    }
+    return true;
+            
+}
