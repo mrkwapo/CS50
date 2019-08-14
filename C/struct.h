d6b6e48af5f2cde888e577a1c481db8fa0ead12f
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-14 11:18:34 -0400

    Create struct.h

---
+++ C/struct.h
@@ -0,0 +1,13 @@
+// Represents a student
+//essentially creating a custom datatype in a custom header file called "struct.h"
+
+typedef struct
+{
+    //specifying what the structure should have
+    char *name;
+    char *major;
+    char *dorm;
+    char *room;
+}
+//name of this custom datastructure
+student;
