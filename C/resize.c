a83c4b454397ad46a9c32832ffbfc79793f68c1d
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-22 07:29:05 -0400

    Update resize.c

--- C/resize.c
+++ C/resize.c
@@ -1,34 +1,114 @@
-// CS50 IDE crashed so I copied what I had so far. This is incomplete
+//Use ./resize number originalfile resizedfilename to run this program
+// Resizes a BMP file
 
 #include <stdio.h>
-#include <cs50.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
 
-int get_positive_int(string second);
+#include "bmp.h"
 
-int main(int argc, string argv[])
+int main(int argc, char *argv[])
 {
-    if (!(argc == 4 && validate_arguement(argv[1])))
+    // ensure proper usage
+    if (argc != 4)
     {
-        printf("incorrect usage\n");
-     return 1;
+        printf("Usage: copy infile outfile\n");
+        return 1;
     }
 
+    //handle resize factor
+    int n = atoi(argv[1]);
 
-}
+    // remember filenames
+    char *infile = argv[2];
+    char *outfile = argv[3];
 
-
-//User must enter a positive integer between 1 and 100 as the second argument
-int get_positive_int(string second)
-{
-    string second = argv[1];
-    do
+    // open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
     {
-         printf("incorrect usage\n");
-     return 1;
+        printf("Could not open %s.\n", infile);
+        return 2;
     }
-    // reprompts user if they do not input a positive integer
-    while (second < 1 && second > 100);
-    return n;
+
+    // open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        printf("Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        printf("Unsupported file format.\n");
+        return 4;
+    }
+
+        // determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    //Modify headers before writing to outptr
+    //Create new file headers
+    BITMAPFILEHEADER new_bf = bf;
+    BITMAPINFOHEADER new_bi = bi;
+
+    new_bi.biWidth *= n;
+    new_bi.biHeight *= n;
+    int new_padding = (4 - (new_bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    new_bi.biSizeImage = (new_bi.biWidth + new_padding)*(bi.biHeight);
+
+
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&new_bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&new_bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        // iterate over pixels in scanline
+        for (int j = 0; j < bi.biWidth; j++)
+        {
+            // temporary storage
+            RGBTRIPLE triple;
+
+            // read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr); //[R G padding]
+
+            // write RGB triple to outfile n times and handle width
+            fwrite(&triple, sizeof(RGBTRIPLE), n, outptr); //[R R R G G G new_padding]
+        }
+
+        // skip over padding, if any
+        fseek(inptr, padding, SEEK_CUR);
+
+        // then add it back (to demonstrate how). Adding new padding
+        for (int k = 0; k < new_padding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+    }
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
+
+    // success
+    return 0;
 }
