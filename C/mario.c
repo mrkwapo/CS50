c80939eb9fa5ebf75dd5730ed59ad1b1c8a8a42e
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-11-16 22:38:29 -0500

    done!

--- C/mario.c
+++ C/mario.c
@@ -1,4 +1,4 @@
-//must complete
+//this program asks for the height of a Nintendo Mario pyramid from the user and then creates it using hashes.
 #include <cs50.h>
 #include <stdio.h>
 
@@ -6,28 +6,26 @@ int get_positive_int(string prompt);
 
 int main(void)
 {
+    //asking user for the height
     int height = get_positive_int("Height: ");
-    
-        for(int i= height; i > 0; i--)
+    //This is the main loop loop that handle the height of each pyramid
+    for (int i = 0; i < height; i++)
     {
-        
-        for (int j = 0; j < i; j++)
-        {     
-        printf(".");
+        //this loop uses the given height minus one to create the spaces needed to justify/align the hashes to the right
+        for (int k = 1; k < height - i; k++)
+        {
+            printf(" ");
         }
-        printf("\n");
-            
-    }
-    
-    for(int i= 0; i < height; i++)
-    {
+        //this loop prints the amount of hashes for each row based on each iteration for the given height
         for (int j = 0; j <= i; j++)
-        {     
-        printf("#");
+        {
+            printf("#");
         }
+        //this creates a new line after each iteration of the outer for loop
         printf("\n");
+       
     }
-
+    
 }
 
 // Prompt user for positive integer
