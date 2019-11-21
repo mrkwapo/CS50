41babde9867f3c0017abd2059c9b83f3cf322d93
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-11-20 19:53:17 -0500

    Create cast2.c

---
+++ C/cast2.c
@@ -0,0 +1,12 @@
+//this program was created to demonstrate 'cast' which is a verb that describes converting one data type to another.
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int cast1 = 72;
+    int cast2 = 73;
+    int cast3 = 33;
+
+    printf("%c %c %c\n", (char) cast1, (char) cast2, (char) cast3);
+}
