78884ff837d54641cca53020e588f28878a59c3a
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-11-20 19:07:40 -0500

    Create buggy.c

---
+++ C/buggy.c
@@ -0,0 +1,22 @@
+//this program was used to practice using cs50 tools.
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for(int i = 0; i < 10; i++)
+    {
+        printf("#\n");
+    }
+}
+
+
+
+//The following are CS50 tools:
+
+//help50 correctness
+//printf correctness
+//debug50 correctness
+//check50 correctness
+//style50  style
