bf664733d9cf38e0795fc9b1ea978ccfe64af142
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-11-30 08:26:15 -0500

    just need to preserve spaces and punctuation

--- C/vigenerex.c
+++ C/vigenerex.c
@@ -1,3 +1,4 @@
+//This program uses a keyword to encrypt a message. Each character in the keyword shifts a letter in a message in order. And the keyword repeats while the message continues to be encrypted.
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
@@ -7,40 +8,48 @@ int shift(char c);
 int main(int argc, string argv[])
 {       
     //do not run if there is no argument or more than one argument
-    if(argc !=2)
+    if (argc != 2)
     {
         printf("./vigenere keyword \n");
         return 1;
     }
  
     //storing the value of argument index 1 in a variable
-     string keyword = argv[1];
+    string keyword = argv[1];
     
-    //checking each letter in the keyword. If there is a number stop program
+    //checking each letter in the keyword. If there is a number or punctutation, the program will not run
     for (int i = 0, len = strlen(keyword); i < len; i++)
     {
-        if(isdigit(keyword[i]))
+        if (isdigit(keyword[i]))
         {
             printf("./vigenere keyword \n");
             return false;
         }
         
-        if(ispunct(keyword[i]))
+        if (ispunct(keyword[i]))
         {
             printf("invalid keyword \n");
             return false;
         }
     }
     
-    int key = shift(argv[1][0]);
-
     //ask the user for a plain text message
     string plaintext = get_string("Plaintext: ");
     printf("Ciphertext: ");
-
-    for( int i = 0, len = strlen(plaintext); i < len; i++)
+    
+    //initializing a counter for the keyword. Note that the counter uses modulo to repeat the order of shifts using the keyword
+    int count = 0;
+    //creating 2 loops to iterate through the plaintext and keyword
+    for (int i = 0, len = strlen(plaintext); i < len; i++)
     {
-        printf("%c", key + plaintext[i]);
+        for (int j = 0, keyLen = strlen(keyword); j < keyLen; j++)
+        {
+            count = keyword[j] % keyLen;
+            
+        }  
+        //encrypt according to jth character in the keyword which handles wrap around with modulo
+        printf("%c", count + plaintext[i]);
+        
     }
     
     printf("\n");
@@ -48,16 +57,21 @@ int main(int argc, string argv[])
 
 int shift(char c)
 {
-    while(isupper(c)) 
+    //if the character is uppercase we minus it by the ASCII value of 'A' so that 'A' = 0, 'B' = 2, etc.
+    while (isupper(c)) 
     {
         int value = c - 65;
         return value;
     }
-    while(islower(c)) 
+    
+    //if the character is lowercase we minus it by the ASCII value of 'a' so that 'a' = 0, 'b'= 2, etc
+    while (islower(c)) 
     {
         int value = c - 97;
         return value;
     }
+    
+    
     return 0;
 }
 
