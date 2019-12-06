fa5ef443f8c81455ba569a34304a35a0b9d53dcc
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-06 07:32:18 -0500

    still working on resize

--- C/resizex.c
+++ C/resizex.c
@@ -4,25 +4,27 @@
 #include <string.h>
 #include <ctype.h>
 
+#include "bmp.h"
+
 int main(int argc, string argv[])
 {
     //declaring a variable to represnt the number needed in argv[1]
     int n = atoi(argv[1]);
-    
+
     //validating we have 3 command line arguments
     if (argc != 4)
     {
         printf("4 arguments only Usage: ./resize n infile outfile \n");
         return 1;
     }
-    
+
     //validating the number is positive but less than or equal to 100
     if (n <= 0 || n > 100)
     {
         printf("n must be between 0-101 Usage: ./resize n infile outfile \n");
         return 1;
     }
-    
+
     for (int i = 0, len = strlen(argv[1]); i < len; i++)
     {
         if(!isdigit(argv[1][i]))
@@ -31,16 +33,16 @@ int main(int argc, string argv[])
         return 1;
         }
     }
-    
+
      // Declaring file names
     char *infile = argv[2];
     char *outfile = argv[3];
-    
-    // open input file
+
+  // open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
-        printf("Could not open %s.\n", infile);
+        fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
 
@@ -49,11 +51,11 @@ int main(int argc, string argv[])
     if (outptr == NULL)
     {
         fclose(inptr);
-        printf("Could not create %s.\n", outfile);
+        fprintf(stderr, "Could not create %s.\n", outfile);
         return 3;
     }
-    
-        // read infile's BITMAPFILEHEADER
+
+    // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
 
@@ -67,9 +69,28 @@ int main(int argc, string argv[])
     {
         fclose(outptr);
         fclose(inptr);
-        printf("Unsupported file format.\n");
+        fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
+    // determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+bi.biWidth *= n;
+bi.biHeight *= n;
+bi.biSizeImage = ((sizeof(RGBTRIPLE) * bi.biWidth) + padding) * abs(bi.biHeight);
+bf.bfSize = bi.biSizeImage* + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+
+    printf("%i  bytes\n",bf.bfSize); //total size of file in bytes which includes pixels, padding and headers
+    //bf.bfSize = bi.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+    printf("%i bytes\n",bi.biSizeImage); // total size of image in bytes which includes pixels and padding.
+    //bi.biSizeImage = ((sizeof(RGBTRIPLE) * bi.biWidth) + padding) * abs(bi.biHeight);
+
+    printf("%i pixels wide\n",bi.biWidth); //width of image in pixels not including padding
+
+    printf("%i pixels tall\n",bi.biHeight); //height of image in pixels
+
     
     // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
@@ -77,18 +98,38 @@ int main(int argc, string argv[])
     // write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
-   
+    // iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        // iterate over pixels in scanline
+        for (int j = 0; j < bi.biWidth; j++)
+        {
+            // temporary storage
+            RGBTRIPLE triple;
 
-            
-           
-            
+            // read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+            // write RGB triple to outfile
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+        }
+
+        // skip over padding, if any
+        fseek(inptr, padding, SEEK_CUR);
+
+        // then add it back (to demonstrate how)
+        for (int k = 0; k < padding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+    }
+
         // close infile
     fclose(inptr);
 
     // close outfile
     fclose(outptr);
-    
-    // Free memory
-//     free(t);
+
+
     return 0;
 }
