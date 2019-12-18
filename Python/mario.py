1cb3e73f4349f0d782037ca311acd158d832004a
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-18 11:08:50 -0500

    done!

--- Python/mario.py
+++ Python/mario.py
@@ -1,3 +1,5 @@
+# this program creates a nintendo mario like pyramid by first asking the user for the height. And the pyramid blocks are represented by hashes 
 height = int(input("height: "))
 for x in range(0,height+1,1):  
   print(x * "#") 
+  print(((height - x)-1) * " ", end ='')
