457d71ae09ed97cda5fd24162617821e3f16f6d5
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-19 11:36:46 -0500

    Update caesar.py

--- Python/caesar.py
+++ Python/caesar.py
@@ -7,7 +7,17 @@ print(type(sys.argv[0:]) is list)
 # print(type(sys.argv[0:]) is list)
 
 #validating length of command line arguments
-if not len(str(sys.argv)) == 2:
-  print("Usage: python caesar.py n") 
-  exit(1)
+# if not len(str(sys.argv)) == 1:
+#   print("Usage: python caesar.py key") 
+#   exit(1)
 
+#Printing out plaintext without new license
+plaintext = int(input("plaintext: "))
+print("cyphertext: ")
+key = 1
+
+for i in range(0, len(plaintext),1):
+  c = int(key) % 26
+  if plaintext[i].islower():
+    if plaintext[i] + c < 122:
+      print(plaintext[i]+ c)
