70d543b890fb67a09c9d5963fc1caab6e986a870
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-18 22:20:25 -0500

    imported cs50 library

--- Python/mario2.py
+++ Python/mario2.py
@@ -1,6 +1,8 @@
 # this program creates a nintendo mario like pyramid by first asking the user for the height. And the pyramid blocks are represented by hashes
-height = int(input("height: "))
-if not height > 0 and height < 9:
+import cs50
+
+height = cs50.get_int("height: ")
+while height < 1 or height > 8:
   height = int(input("height: "))
 
 for x in range(0,height+1,1):
