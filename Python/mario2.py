bd141d7be8116ebbf14719557681a5a47b2acdf2
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-18 23:05:34 -0500

    done with results and validation

--- Python/mario2.py
+++ Python/mario2.py
@@ -1,10 +1,11 @@
 # this program creates a nintendo mario like pyramid by first asking the user for the height. And the pyramid blocks are represented by hashes
 import cs50
 
-height = cs50.get_int("height: ")
-while height < 1 or height > 8:
-  height = int(input("height: "))
+while True:
+  height = cs50.get_int("height: ")
+  if height > 0 and height < 9:
+    break
 
 for x in range(0,height+1,1):
   print(x * "#")
-  print(((height - x)-1) * " ", end ='')
+  print(((height - x)-1) * " ", end ="")
