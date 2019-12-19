fafcfbc8e8b9ad67b2a3949409c4ae52eb27c1f1
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-19 00:04:16 -0500

    need to work on validations

--- Python/cash.py
+++ Python/cash.py
@@ -1,6 +1,33 @@
-changeOwed = float(input("Change:"))
+import cs50
+import math
 
-while(changeOwed <= 0):
-  changeOwed = float(input("Change:"))
-else:
-  print(changeOwed)
+changeOwed = cs50.get_float("change owed:")
+
+convertedChange = changeOwed * 100
+
+count = 0
+
+while convertedChange >= 25:
+
+        count += math.floor(convertedChange / 25)
+        convertedChange %= 25
+
+
+while convertedChange >= 10 and convertedChange < 25:
+
+        count += math.floor(convertedChange / 10)
+        convertedChange %= 10
+
+
+while convertedChange >= 5 and convertedChange < 10:
+
+        count += math.floor(convertedChange / 5);
+        convertedChange %= 5
+
+while convertedChange >= 1 and convertedChange < 5:
+
+         count += convertedChange / 1
+         convertedChange %= 1
+
+print("Remaining Change " , convertedChange)
+print("Coins Count: " , count)
