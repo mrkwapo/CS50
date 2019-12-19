993a46a095cb337822ee8c72dd5108546bac42f9
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-19 10:02:33 -0500

    Update caesar.py

--- Python/caesar.py
+++ Python/caesar.py
@@ -4,3 +4,10 @@ import sys
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
 print(type(sys.argv[0:]) is list)
+# print(type(sys.argv[0:]) is list)
+
+#validating length of command line arguments
+if not len(str(sys.argv)) == 2:
+  print("Usage: python caesar.py n") 
+  exit(1)
+
