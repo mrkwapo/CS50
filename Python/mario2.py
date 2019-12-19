0f32be82154df276412d7cbafb66a6e652a160ff
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-18 21:54:15 -0500

    working on validations

---
+++ Python/mario2.py
@@ -0,0 +1,8 @@
+# this program creates a nintendo mario like pyramid by first asking the user for the height. And the pyramid blocks are represented by hashes
+height = int(input("height: "))
+if not height > 0 and height < 9:
+  height = int(input("height: "))
+
+for x in range(0,height+1,1):
+  print(x * "#")
+  print(((height - x)-1) * " ", end ='')
