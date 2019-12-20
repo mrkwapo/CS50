731f469a03e18b53e1b6248c07984eb972ffb8d2
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-20 13:46:14 -0500

    Update bleep.py

--- Python/bleep.py
+++ Python/bleep.py
@@ -5,20 +5,22 @@
 # if len(sys.argv) != 2:
 #     print("Usage: python bleep.py banned.txt")
 #     exit(0)
-
-# arg1 = sys.argv[1]
-# arg2 = sys.argv[2]
+   
     
 explicit = input("What message would you like to censor?\n")
-
-# f = open("banned.txt", "r")
-# print(f.read())
-
-
-with open('banned.txt') as f:
-    if explicit in f.read():
-        print("true")
-    else:
-      print("false")
+# arg1 = sys.argv[1]
+# arg2 = sys.argv[2]
 
 
+f = open("banned.txt", "r")
+
+explicitList = explicit.split(' ')
+# with open('banned.txt') as f:
+for i in range(0, len(explicitList), 1):    
+    if explicitList[i] in f.read():
+      print("*" * len(explicitList[i]))
+    # if not explicit[i] in f.read():
+    elif not explicitList[i] in f.read():
+      print(explicitList[i] + ' ', end='')
+
+print()
