6d48d615e765b834cd736bf8e7f05b0cccc450cf
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-21 14:25:41 -0500

    (7/8) Still needs to handle uppercase

--- Python/bleep.py
+++ Python/bleep.py
@@ -1,31 +1,42 @@
-# import sys
-# import cs50
+import sys
+import cs50
 
 # validating length of command line arguments
-# if len(sys.argv) != 2:
-#     print("Usage: python bleep.py banned.txt")
-#     exit(0)
-   
-    
-explicit = input("What message would you like to censor?\n")
-# arg1 = sys.argv[1]
-# arg2 = sys.argv[2]
+if len(sys.argv) != 2:
+    print("Usage: python bleep.py banned.txt")
+    exit(1)
+# Initializing a variable the argument which is the name of the file containing the banned words
+dictionary = sys.argv[1]
 
+# Getting the message that is possibly explicit from the user
+explicit = cs50.get_string("What message would you like to censor?\n")
 
-f = open("banned.txt", "r")
+# Opening the banned words file
+bannedWords = open(dictionary, "r")
 
+# readling the banned words file and organizing the content into a list
+bannedWordsList = bannedWords.read().split('\n')
 
-# print("The original list : " + str(explicit.split(' ')))
+# Converting the explicit message into a list
+explicitList = explicit.split(' ')
 
-for i in range(0, len(explicit.split(' ')), 1): 
-    if not explicit.split(' ')[i] in f.read():
+# converting the explicit list into a set
+explicitSet = set(explicitList)
 
-      res = [sub.replace(explicit.split(' ')[i], ("*" * len(explicit.split(' ')[i]))) for sub in explicit.split(' ')] 
+# Initializing an empty List to append censored words in
+censored = []
 
-# print("The list after substring replacement : " + str(res)) 
+# Iterating through each word in the explicit list.
+for x in explicitList:
+    # If the word is found in the banned.txt replace it with *s that are the same length of the word and append it to the list called censored
+    if x in bannedWordsList:
+        censored.append(x.replace(x, "*" * len(x)))
+    else:
+        # if no sensoring is needed just append it to the List called censored
+        censored.append(x)
 
-s = " "
-
-print (s.join( res ))     
+# converting the List of cesnored words into a string
+print(" ".join(censored))
 
 print()
+
