e406c0506f0fad81e8d1bc92f62ba1dfca677cf8
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-22 22:39:31 -0500

    Update bleepx.py

--- Python/bleepx.py
+++ Python/bleepx.py
@@ -1,30 +1,38 @@
 import sys
 import cs50
 
-# validating length of command line arguments
+# validating the amount of command-line arguments
 if len(sys.argv) != 2:
     print("Usage: python bleep.py banned.txt")
-    exit(0)
-
+    exit(1)
+# Initializing a variable the argument which is the name of the text file containing the banned words
 dictionary = sys.argv[1]
 
+# Getting the message that is possibly explicit from the user
 explicit = cs50.get_string("What message would you like to censor?\n")
 
+# Opening the banned words file
 bannedWords = open(dictionary, "r")
 
-bannedWordsList= bannedWords.read().split('\n')
+# reading the banned words file and organizing the content into a list
+bannedWordsList = bannedWords.read().split('\n')
 
+# Converting the explicit message into a list
 explicitList = explicit.split(' ')
 
-explicitSet = set(explicitList)
+# Initializing an empty List to append censored words in
+censored = []
 
-# print(bannedWords.read())
+# Iterating through each word in the explicit list.
+for x in explicitList:
+    # If the word is found in the banned.txt replace it with *s that are the same length of the word and append it to the list called censored
+    if x.casefold() in bannedWordsList:
+        censored.append(x.replace(x, "*" * len(x)))
+    else:
+        # if no sensoring is needed just append it to the List called censored
+        censored.append(x)
 
-# converted the message that needs to be censored, and the banned words file content into  different set
-for x in explicitSet:
-    # print(explicitSet)
-    if x in bannedWordsList:
-        print(x)
+# converting the List of cesnored words into a string
+print(" ".join(censored))
 
 print()
-
